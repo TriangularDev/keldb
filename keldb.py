eea852fd065ddba8e536c45ee1086bd7fca3159e
TriangularDev <72287094+TriangularDev@users.noreply.github.com>
2026-05-31 17:16:57 -0400

    Add base types

--- keldb.py
+++ keldb.py
@@ -1,1 +1,20 @@
+from enum import Enum
+import asyncio
 
+class Enums():
+    class InformationAvailability(Enum):
+        NONE = 1
+        VALIDATED = 2
+        FULL = 3
+
+class Node():
+    pass
+
+class KelDB(Node):
+    pass
+
+class Hook():
+    pass
+
+class FileStoreHook(Hook):
+    pass
